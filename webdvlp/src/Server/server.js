744da38c3605009b8227d9692c518026bfa43a63
Dayenkai <BawiluEnt@hotmail.com>
2019-10-17 19:48:50 +0200

    Finalement, LE SERVEURè_é !

---
+++ webdvlp/src/Server/server.js
@@ -0,0 +1,51 @@
+// Imports
+var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+var server = express();
+
+//DAMN MA MAN
+
+server.use(bodyParser.json());
+// Instantiate server
+var uri = 'mongodb://localhost:27017/tutoriel'
+
+//Import Routes
+const postsRoute = require('./routes/posts');
+
+server.use('/posts', postsRoute);
+
+
+//Configure Routes
+server.get("/", function(req, res){
+    res.setHeader('Content-Type', 'text/plain')
+    res.status(200).send('Welcome home');
+});
+
+
+
+// Connect to DB
+mongoose.connect('mongodb://localhost:27017/tutoriel', 
+{ useNewUrlParser: true, useUnifiedTopology: true},
+ function (err, db){
+    console.log("Connected to tutoriel Mongo Database");
+    });
+
+
+//Listening to the Server
+server.listen(8080);
+
+
+// Lauch Server
+/*server.listen(8080, function(){
+    console.log("Server listening on port 8080\nWaiting for actions...");
+    MongoClient.connect(uri, function (err, db){
+        console.log("Connected to tutoriel Mongo Database");
+        var dbo = db.db('tutoriel').collection("personnages").find();
+        dbo.each(function(err, doc){
+            console.log(doc);
+        })
+        db.close();
+    });
+});*/
