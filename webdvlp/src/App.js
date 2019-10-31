d7a9b304c6128189e9aeea86920c29b432c44a7b
AlexSarah <sarah.alexandre14@gmail.com>
2019-10-31 11:08:06 +0100

    Sidebar

--- webdvlp/src/App.js
+++ webdvlp/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Login from './front/Login.js';
 import Header from './front/Header.js';
 import Footer from './front/Footer.js';
+import SideBar from "./front/Sidebar.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <SideBar />
         <Header/>
         <h1>Choose Your Seat !</h1>
         <Login/>
