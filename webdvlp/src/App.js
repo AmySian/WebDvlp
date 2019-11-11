a372b97af08de0de71136fc8641ddda8b482bdc7
Dayenkai <BawiluEnt@hotmail.com>
2019-11-11 17:44:44 +0100

    Les routes sont protegees ; J'ai du mettre tout le home.js dans dans app.js pour que ca marche, c'est horrible. Du coup, en plus des diagrammes d'emy, il y aura peut etre en plus le widget avec la selection des places

--- webdvlp/src/App.js
+++ webdvlp/src/App.js
@@ -1,21 +1,140 @@
 import React, { Component } from 'react'
 import './App.css';
 import SideBar from "./front/Sidebar.js";
+import Header from './front/Header.js';
+import Footer from './front/Footer.js';
 import About from "./front/About.js";
-import Home from "./front/Home.js";
 import Dashboard from "./front/Dashboard.js"
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import {BrowserRouter as Router,Switch,Route, Link, Redirect, withRouter} from "react-router-dom";
+
+
+const ProtectedRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={(props) => (
+
+     fakeAuthCentralState.isAuthenticated === true ? 
+        <Component {...props} /> : <Redirect to={{ pathname: '/', state: { from: props.location }}} />   
+  )} />
+);
+
+const fakeAuthCentralState = {
+  isAuthenticated: false,
+  authenticate() {
+     this.isAuthenticated = true;
+  },
+  signout() {
+     this.isAuthenticated = false; 
+  }
+};
+
+class Home extends Component {
+
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          email: "",
+          password: "",
+          redirectToReferrer: false
+        };
+      }
+      
+
+    handleChange = event => {
+        this.setState({
+          [event.target.name]: event.target.value
+        });
+      }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        const user = {
+          user: this.state.email,
+          password: this.state.password
+        }
+        const a = new Headers();
+        console.log();
+        fetch('http://localhost:8080/posts/users/', {
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+        method: "POST",
+        body: JSON.stringify(user)
+        }).then(res=>{
+            console.log(res);
+          res.json().then(result=>{
+            if (result.validation === "User found, Connected")
+            {
+              console.log("redirect to reffer "+this.state.redirectToReferrer);
+              console.log("variable d'authentifsication " +fakeAuthCentralState.isAuthenticated)
+               fakeAuthCentralState.authenticate();
+                this.setState(() => ({
+                   redirectToReferrer: true
+                }));
+                console.log("Je suis là pd");
+             console.log("variable d'authentification " +fakeAuthCentralState.isAuthenticated)
+             console.log("redirect to reffer "+this.state.redirectToReferrer);
+              //window.location="/dashboard";
+            }
+          });
+        }).catch(err=>{
+          console.log("Il y a une erreur d'envoi");
+        });
+      }
+
+    handleClick = event => {
+      }
+
+    render() {
+
+      const Public = () => <h3>Public Content</h3>;
+     const Protected = () => <h3>Protected Content</h3>;
+
+      const { from } = this.props.location.state || { from: { pathname: '/' } };
+      const { redirectToReferrer } = this.state;
+if ( redirectToReferrer === true){
+  console.log("Motherfucker");
+        return(<Redirect to="/dashboard"/>)
+         }         /*this.props.history.push(from.pathname);
+      }*/
+
+
+        return (             
+        <div>
+            <Header/>
+                <div className="Login">
+                    <h1>Choose your Seat !</h1>
+                    <form onSubmit={this.handleSubmit}>
+                        <h3>Email</h3>
+                        <input type="email" name="email" minLength="1" onChange={this.handleChange} />
+                        <h3>Password</h3>
+                        <input type="password" name="password" minLength="3" onChange={this.handleChange} />
+                        <input type='submit' value="Log in" onClick={this.handleClick} />
+                    </form>
+
+                </div>
+            <Footer/>
+        </div>
+        )
+    }
+}
+
+
+
+
+
 
 class App extends Component {
   render() {
+    console.log(this.props.children+"Hello there")
     return (
       <Router>
         <div className="App">
           <SideBar />
           <Switch>
             <Route path="/about" component={About} />
-            <Route path="/dashboard" component={Dashboard} />
             <Route path="/" exact component={Home} />
+            <ProtectedRoute path="/dashboard" exact component={Dashboard} />
           </Switch>
         </div>
       </Router>
--- webdvlp/src/front/Home.js
+++ webdvlp/src/front/Home.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react'
 import Header from './Header.js';
 import Footer from './Footer.js';
+import Dashboard from "./Dashboard.js"
+import {BrowserRouter as Route,  Redirect} from 'react-router-dom'
 
+
+const ProtectedRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={(props) => (
+     fakeAuthCentralState.isAuthenticated === true ? 
+        <Component {...props} /> : <Redirect to={{ pathname: '/login', state: { from: props.location }}} />   
+  )} />
+);
+
+const fakeAuthCentralState = {
+  isAuthenticated: false,
+  authenticate() {
+     this.isAuthenticated = true;
+  },
+  signout() {
+     this.isAuthenticated = false; 
+  }
+};
 
 class Home extends Component {
 
@@ -11,8 +30,10 @@ class Home extends Component {
         this.state = {
           email: "",
           password: "",
+          redirectToReferrer: false
         };
       }
+      
 
     handleChange = event => {
         this.setState({
@@ -27,7 +48,7 @@ class Home extends Component {
           password: this.state.password
         }
         const a = new Headers();
-        console.log(a);
+        console.log();
         fetch('http://localhost:8080/posts/users/', {
           headers: {
             'Accept': 'application/json',
@@ -40,7 +61,16 @@ class Home extends Component {
           res.json().then(result=>{
             if (result.validation === "User found, Connected")
             {
-              window.location="/dashboard";
+              console.log("redirect to reffer "+this.state.redirectToReferrer);
+              console.log("variable d'authentifsication " +fakeAuthCentralState.isAuthenticated)
+               fakeAuthCentralState.authenticate();
+                this.setState(() => ({
+                   redirectToReferrer: true
+                }));
+                console.log("Je suis là pd");
+             console.log("variable d'authentification " +fakeAuthCentralState.isAuthenticated)
+             console.log("redirect to reffer "+this.state.redirectToReferrer);
+              //window.location="/dashboard";
             }
           });
         }).catch(err=>{
@@ -52,7 +82,27 @@ class Home extends Component {
       }
 
     render() {
-        return (
+
+      /*const Public = () => <h3>Public Content</h3>;
+     const Protected = () => <h3>Protected Content</h3>;
+
+
+
+const { from } = this.props.location.state || { from: { pathname: '/' } };
+      const { redirectToReferrer } = this.state;
+if (redirectToReferrer === true) {
+         this.props.history.push(from.pathname);
+      }*/
+      const { from } = this.props.location.state || { from: { pathname: '/' } };
+      const { redirectToReferrer } = this.state;
+if ( redirectToReferrer === true){
+  console.log("Motherfucker");
+        return(<Redirect to="/dashboard"/>)
+         }         /*this.props.history.push(from.pathname);
+      }s*/
+
+
+        return (             
         <div>
             <Header/>
                 <div className="Login">
@@ -63,7 +113,9 @@ class Home extends Component {
                         <h3>Password</h3>
                         <input type="password" name="password" minLength="3" onChange={this.handleChange} />
                         <input type='submit' value="Log in" onClick={this.handleClick} />
+                        <ProtectedRoute path="/dashboard" component={Dashboard} />
                     </form>
+
                 </div>
             <Footer/>
         </div>
